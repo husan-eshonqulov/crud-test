d96c509480678abf3c5751d0ecb76fef65bba41f
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-01-26 11:22:41 +0500

    add static js file

---
+++ public/admin/js/index.js
@@ -0,0 +1,2 @@
+const editBtns = document.querySelectorAll('.edit-btn');
+const deleteBtns = document.querySelectorAll('.delete-btn');
--- public/js/admin.js
+++
@@ -1,3 +0,0 @@
-// const editBtn = document.querySelector('.edit-btn');
-// const deleteBtn = document.querySelector('.delete-btn');
-console.log('hello');
