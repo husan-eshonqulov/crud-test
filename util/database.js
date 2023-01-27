ebd27e11dad4b2891a6c5e88826d37c49c6e050f
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-01-27 19:31:43 +0500

    add template

--- util/database.js
+++ util/database.js
@@ -7,4 +7,16 @@ const pool = mysql.createPool({
   password: process.env.PASSWORD,
 });
 
+// pool.execute(`CREATE TABLE 'crud-test'.'users' (
+//   'id' INT NOT NULL AUTO_INCREMENT,
+//   'firstname' VARCHAR(45) NOT NULL,
+//   'lastname' VARCHAR(45) NOT NULL,
+//   'username' VARCHAR(45) NOT NULL,
+//   'password' VARCHAR(60) NOT NULL,
+//   'createdAt' DATETIME NOT NULL,
+//   'updatedAt' DATETIME NOT NULL,
+//   PRIMARY KEY ('id'),
+//   UNIQUE INDEX 'id_UNIQUE' ('id' ASC) VISIBLE,
+//   UNIQUE INDEX 'username_UNIQUE' ('username' ASC) VISIBLE)`);
+
 module.exports = pool.promise();
